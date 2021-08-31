70875bd0ceca04723415be6e30eac6979630f780
einwille <einwil01@ads.uni-passau.de>
2021-08-31 22:04:07 +0200

    Update modules

---
+++ hs/H01-getting-started.hs
@@ -0,0 +1,51 @@
+module H01 where
+
+-- comment that occupies the part of the line to the right of the symbol
+
+{-
+nested comment (arbitrary length
+
+GHCi cmds:
+:l      load module
+:r      repeat last :l cmd
+:t exp  give type of expression
+:i name give information about name
+:q      quit
+:h      help
+:e file edit file
+
+:set editor vi      set the editor to be vi
+lets s = exp        give s the value of exp within this session (temporary)
+
+ctrl+L == :!clear   clear
+
+:browse Prelude     list of all the functions in the Prelude.hs module
+
+-}
+
+--  The value life is an Integer, defined to be 42.
+
+life :: Integer
+life = 42
+
+--  The function to square an integer.
+
+square :: Integer -> Integer
+square n = n*n
+
+--  The function to double an Integer
+
+double :: Integer -> Integer
+double n = 2*n
+
+--  An example using double, square and life
+
+example :: Integer
+example = double (life - square (2+2))
+
+--  functionial composition
+
+example2 :: [Integer]
+example2 = filter (not . even) [1..10] --   odd integers from 1 to 10
+
+
