c071e4c98cdf0e2738e20101747e28b04b8e6cb8
einwille <einwil01@ads.uni-passau.de>
2021-09-01 01:16:03 +0200

    Add ascii art module usage

--- hs/01-getting-started/UsePictures.hs
+++ hs/01-getting-started/UsePictures.hs
@@ -4,4 +4,21 @@ module UsePictures where
 import Pictures
 --import PicturesSVG
 
+--  printPicture (rotateHorse horse)
+rotateHorse :: Picture -> Picture
+rotateHorse h = rotate h
 
+--  printPicture (invertHorseColour horse)
+invertHorseColour :: Picture -> Picture
+invertHorseColour h = invertColour h
+
+data Colour = White | Black deriving Show
+
+p = printPicture
+horseBlack = invertHorseColour horse    --  horse is white
+
+-- p (whiteHorse horse White) = horse
+-- p (whiteHorse horseBlack Black) = horse
+whiteHorse :: Picture -> Colour -> Picture
+whiteHorse h White = h
+whiteHorse h Black = invertHorseColour h
